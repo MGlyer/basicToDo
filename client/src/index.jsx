e040a2ac4cce51f38633913c63a01c53cc878253
Martin <MartinGlyer@gmail.com>
2018-07-12 20:26:11 -0400

    includes first webpack compilation

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
-import React from ('react')
-import ReactDom from ('react-dom')
-import axios from ('axios')
+import React from 'react'
+import ReactDom from 'react-dom'
+import axios from 'axios'
 
 class App extends React.Component {
     constructor(props) {
